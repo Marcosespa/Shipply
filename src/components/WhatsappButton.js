3f593c8dea4cd01ac8e7f8bad435a0b019e93617
Marcosespa <marcosespa24@gmail.com>
2025-02-10 14:45:15 -0500

    Intento boton de Wha

--- src/components/WhatsappButton.js
+++ src/components/WhatsappButton.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/whatsappButton.css";
 
 const WhatsappButton = () => {
+  const [position, setPosition] = useState(700); // Posición inicial desde la parte superior
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setPosition(window.scrollY + 300); // Ajusta para moverse con el scroll
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <a
       href="https://wa.me/573184516959"
       target="_blank"
       rel="noreferrer"
       className="whatsapp-button"
+      style={{ top: `${position}px` }} // Ahora usamos 'top' en lugar de 'bottom'
     >
       <i className="bi bi-whatsapp"></i>
     </a>
